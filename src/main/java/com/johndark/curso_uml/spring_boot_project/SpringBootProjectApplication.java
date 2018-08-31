1691b3f73a18e1e51db0f879285841f9d7f10d5d
Airton Vieira dos Santos <airton.santos3.1415@gmail.com>
2018-08-31 10:04:38 -0300

    Projeto inicializado

---
+++ src/main/java/com/johndark/curso_uml/spring_boot_project/SpringBootProjectApplication.java
@@ -0,0 +1,12 @@
+package com.johndark.curso_uml.spring_boot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/johndark/curso_uml/spring_boot_project/SpringBootProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.johndark.curso_uml.spring_boot_project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
