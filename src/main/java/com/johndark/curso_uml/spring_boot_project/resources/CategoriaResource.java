770e92a71c3228ce328e8377d9e4c80ccc6aa2f3
Airton Vieira dos Santos <airton.santos3.1415@gmail.com>
2018-09-01 09:13:02 -0300

    Criado a primeira class de domínio

--- src/main/java/com/johndark/curso_uml/spring_boot_project/resources/CategoriaResource.java
+++ src/main/java/com/johndark/curso_uml/spring_boot_project/resources/CategoriaResource.java
@@ -5,6 +5,9 @@
  */
 package com.johndark.curso_uml.spring_boot_project.resources;
 
+import com.johndark.curso_uml.spring_boot_project.domain.Categoria;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +22,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
     
     @RequestMapping(method=RequestMethod.GET)
-    public String list(){
+    public List<Categoria> list(){
         
-        return "O Reste está funcionando";
+        Categoria cat1 = new Categoria(1, "Informática");        
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+        
+        return lista;
     }
 }
