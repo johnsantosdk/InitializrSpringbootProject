75572154930a1b32408a4c41aa3a5325a7ea9c7f
Airton Vieira dos Santos <airton.santos3.1415@gmail.com>
2018-09-19 09:00:25 -0300

    ith '#' will be ignored, and an empty message aborts the commit.

--- src/main/java/com/johndark/curso_uml/spring_boot_project/SpringBootProjectApplication.java
+++ src/main/java/com/johndark/curso_uml/spring_boot_project/SpringBootProjectApplication.java
@@ -1,12 +1,30 @@
 package com.johndark.curso_uml.spring_boot_project;
 
+import com.johndark.curso_uml.spring_boot_project.domain.Categoria;
+import com.johndark.curso_uml.spring_boot_project.repositories.CategoriaRepository;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootProjectApplication {
-
+public class SpringBootProjectApplication implements  CommandLineRunner {
+       
+        @Autowired
+        private CategoriaRepository categoriaRepository;
+        
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProjectApplication.class, args);
 	}
+        
+        @Override
+        public void run(String ... args) throws Exception {
+            
+            Categoria cat1 = new Categoria(null, "Informática");
+            Categoria cat2 = new Categoria(null, "Escritório");
+            
+            categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+            
+        }
 }
